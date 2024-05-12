7c6d1bfa79ec7b3ac0506637050d3ccfc1bff68e
ZehraaMA <zahraa@thecodepeople.io>
2024-05-12 13:00:23 +0300

    Implement JWT Authentication Middleware - Created authentication middleware to handle JWT user and token authentication and verification. - Updated user and product routes to utilize JWT authentication instead of passport.

--- products/product.routes.js
+++ products/product.routes.js
@@ -1,31 +1,35 @@
 const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+
 const {
   getAllProducts,
   createProduct,
   deleteProduct,
   updateProduct,
 } = require("../products/product.controllers");
-const passport = require("passport");
-
-const router = express.Router();
+const { authenticateToken } = require("../middlewares/auth");
 
 router.get("/", getAllProducts);
 
 router.post(
   "/",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
+  authenticateToken,
   createProduct
 );
 
 router.put(
   "/:id",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
+  authenticateToken,
   updateProduct
 );
 
 router.delete(
   "/:id",
-  passport.authenticate("jwt", { session: false }),
+  // passport.authenticate("jwt", { session: false }),
+  authenticateToken,
   deleteProduct
 );
 
--- users/user.routes.js
+++ users/user.routes.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const passport = require("passport");
+// const passport = require("passport");
+
 const { signIn, signup } = require("../users/user.controllers");
+const { authenticateUser } = require("../middlewares/auth");
 
 router.post("/signup", signup);
 
 router.post(
   "/sign-in",
-  passport.authenticate("local", { session: false }),
+  // passport.authenticate("local", { session: false }), // Using passport to authenticate users
+  authenticateUser, // using jwt to authenticate users
   signIn
 );
 
